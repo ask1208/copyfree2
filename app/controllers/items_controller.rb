d1f8227fd58190a509a52061f752f83cccf6c580
ask1208 <t.ask.128@icloud.com>
2020-02-24 19:46:04 +0900

    payjs

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -5,5 +5,26 @@ class ItemsController < ApplicationController
 
   def show
   end
+
+  def pay
+    @item = Item.find(params[:id])
+    Payjp.api_key = ENV['PAYJP_PRIVATE_KEY']
+    charge = Payjp::Charge.create(
+    amount: @item.price,
+    card: params['payjp-token'],
+    currency: 'jpy'
+    )
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(
+      :name,
+      :text,
+      :price,
+      #この辺の他コードは関係ない部分なので省略してます
+    ).merge(user_id: current_user.id)
+  end
   
 end
